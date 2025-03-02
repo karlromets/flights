c2393bbb01193dba682e2961e8b7b0482ace2224
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-02 20:26:45 +0200

    feat(model): add city model
    - attach it to airport entity as well

--- backend/src/main/java/com/cgi/flights/model/City.java
+++ backend/src/main/java/com/cgi/flights/model/City.java
@@ -5,13 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 import lombok.Data;
 
 @Data
 @Entity
-public class Airport {
+public class City {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -19,11 +20,6 @@ public class Airport {
   @Column(unique = true)
   private String name;
 
-  @ManyToOne
-  @JoinColumn(name = "country_id")
-  private Country country;
-
-  @ManyToOne
-  @JoinColumn(name = "city_id")
-  private City city;
+  @OneToMany(mappedBy = "city")
+  private List<Airport> airports;
 }
