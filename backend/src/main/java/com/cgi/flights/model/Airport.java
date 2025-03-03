ae0ed3857d0b50e01ab12d2b7871010f5002797a
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-03 17:25:45 +0200

    feat(model): add Flight relationships to Airport

--- backend/src/main/java/com/cgi/flights/model/Airport.java
+++ backend/src/main/java/com/cgi/flights/model/Airport.java
@@ -1,5 +1,7 @@
 package com.cgi.flights.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -26,4 +29,10 @@ public class Airport {
   @ManyToOne
   @JoinColumn(name = "city_id")
   private City city;
+
+  @OneToMany(mappedBy = "departureAirport")
+  private List<Flight> departingFlights;
+
+  @OneToMany(mappedBy = "arrivalAirport")
+  private List<Flight> arrivingFlights;
 }
