dad9fff3267bc140217715791be79b71a022287d
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-04 20:26:05 +0200

    docs(api): add swagger

--- backend/src/main/java/com/cgi/flights/controller/FlightController.java
+++ backend/src/main/java/com/cgi/flights/controller/FlightController.java
@@ -1,24 +1,21 @@
 package com.cgi.flights.controller;
 
+import com.cgi.flights.dto.response.FlightResponseDTO;
+import com.cgi.flights.service.FlightService;
 import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.cgi.flights.dto.response.FlightResponseDTO;
-import com.cgi.flights.service.FlightService;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/flights")
 public class FlightController {
-    private final FlightService flightService;
+  private final FlightService flightService;
 
-    @GetMapping
-    public List<FlightResponseDTO> getFlights() {
-        return flightService.getAllFlights();
-    }
+  @GetMapping
+  public List<FlightResponseDTO> getFlights() {
+    return flightService.getAllFlights();
+  }
 }
