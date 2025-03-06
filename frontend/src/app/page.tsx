5184d2186b7951812a7127662765b00ab3d7e469
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-06 18:15:11 +0200

    feat(frontend): fetch and log flights on home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,19 @@
-export default function Home() {
+import { getFlightController } from "@/api/flight-controller";
+
+async function getFlights() {
+  const flightController = getFlightController();
+  try {
+    const response = await flightController.getFlights();
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const flights = await getFlights();
+  console.log(flights);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-background">
       <h1>Init</h1>
