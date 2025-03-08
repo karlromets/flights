9156c5ffecc3a8a92be70998c6b82328a2bdf94a
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-08 06:59:59 +0200

    feat(flights): add SeatMapper utility class for seat data

--- frontend/src/app/flights/[slug]/page.tsx
+++ frontend/src/app/flights/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getFlightController } from "@/api/flight-controller";
+import { SeatMapper } from "@/lib/utils";
 
 async function getFlight(id: number) {
   const flightController = getFlightController();
@@ -20,5 +21,25 @@ interface FlightPageProps {
 export default async function FlightPage({ params }: FlightPageProps) {
   const { slug } = await params;
   const flight = await getFlight(parseInt(slug));
-  console.log(flight);
+  
+  if(!('plane' in flight) || !flight.plane.seats) {
+    return <h1>Flight details not available</h1>
+  }
+  const seats = flight.plane.seats;
+  const sm = new SeatMapper(seats);
+
+  console.log(seats);
+  console.log("Total", sm.getTotal());
+  console.log("Occupied", sm.getOccupied());
+  console.log("Available", sm.getAvailable());
+  console.log("Columns", sm.getColumns());
+  console.log("Rows", sm.getRows());
+
+  return (
+    <>
+      {/* {flight && flight.plane.seats.map((seat => {
+
+  }))} */}
+    </>
+  );
 }
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,39 @@
+import { SeatResponseDTO } from "@/api/types";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export class SeatMapper {
+  private seats: SeatResponseDTO[];
+
+  constructor(seats: SeatResponseDTO[]) {
+    this.seats = seats;
+  }
+
+  getTotal(): number {
+    return this.seats.length;
+  }
+
+  getOccupied(): number {
+    return this.seats.filter((s) => s.isOccupied).length;
+  }
+
+  getAvailable(): number {
+    return this.seats.filter((s) => !s.isOccupied).length;
+  }
+
+  getColumns(): string[] {
+    const cols = new Set<string>();
+    this.seats.forEach((seat) => cols.add(seat.columnLetter));
+    return Array.from(cols);
+  }
+
+  getRows(): number[] {
+    const cols = new Set<number>();
+    this.seats.forEach((seat) => cols.add(seat.rowNumber));
+    return Array.from(cols);
+  }
 }
