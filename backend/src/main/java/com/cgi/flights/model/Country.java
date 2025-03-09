22b0c24e83b778c41e5e905a8c166876d693a77e
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-09 16:05:23 +0200

    test(service): add unit tests for CountryService

--- backend/src/main/java/com/cgi/flights/model/Country.java
+++ backend/src/main/java/com/cgi/flights/model/Country.java
@@ -1,7 +1,5 @@
 package com.cgi.flights.model;
 
-import java.util.List;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,10 +7,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Country {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ backend/src/test/java/com/cgi/flights/service/CountryServiceTests.java
@@ -0,0 +1,68 @@
+package com.cgi.flights.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.cgi.flights.dto.response.CountryResponseDTO;
+import com.cgi.flights.model.Country;
+import com.cgi.flights.repository.CountryRepository;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CountryServiceTests {
+  @Mock private CountryRepository countryRepository;
+
+  @InjectMocks private CountryService countryService;
+
+  private static final String COUNTRY_1_NAME = "Estonia";
+  private static final Long COUNTRY_1_ID = 1L;
+  private static final String COUNTRY_2_NAME = "Latvia";
+  private static final Long COUNTRY_2_ID = 2L;
+
+  private static final List<Country> COUNTRIES =
+      List.of(
+          Country.builder().id(COUNTRY_1_ID).name(COUNTRY_1_NAME).build(),
+          Country.builder().id(COUNTRY_2_ID).name(COUNTRY_2_NAME).build());
+
+  @Nested
+  @DisplayName("getCountries Tests")
+  class GetCountriesTests {
+    @Test
+    @DisplayName("Should return all countries")
+    void getCountries_WhenCalled_ReturnsAllCountries() {
+      when(countryRepository.findAll()).thenReturn(COUNTRIES);
+
+      List<CountryResponseDTO> result = countryService.getCountries();
+
+      assertAll(
+          () -> assertThat(result).as("checking if the result has size 2").hasSize(2),
+          () ->
+              assertThat(result.get(0).name())
+                  .as("checking if the first country is Estonia")
+                  .isEqualTo(COUNTRY_1_NAME),
+          () ->
+              assertThat(result.get(0).id())
+                  .as("checking if the first country id is 1")
+                  .isEqualTo(COUNTRY_1_ID),
+          () ->
+              assertThat(result.get(1).name())
+                  .as("checking if the second country is Latvia")
+                  .isEqualTo(COUNTRY_2_NAME),
+          () ->
+              assertThat(result.get(1).id())
+                  .as("checking if the second country id is 2")
+                  .isEqualTo(COUNTRY_2_ID),
+          () -> verify(countryRepository, times(1)).findAll());
+    }
+  }
+}
