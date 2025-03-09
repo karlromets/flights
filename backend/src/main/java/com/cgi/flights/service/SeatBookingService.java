8f1b57f2f5d93d6226d47eaa9dc60024d6119ac7
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-09 18:41:55 +0200

    feat(service): add method to retrieve seat bookings for multiple flights

--- backend/src/main/java/com/cgi/flights/service/SeatBookingService.java
+++ backend/src/main/java/com/cgi/flights/service/SeatBookingService.java
@@ -1,20 +1,35 @@
 package com.cgi.flights.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.cgi.flights.model.SeatBooking;
 import com.cgi.flights.repository.SeatBookingRepository;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SeatBookingService {
-    private final SeatBookingRepository seatBookingRepository; 
+  private final SeatBookingRepository seatBookingRepository;
 
-    public List<SeatBooking> getAllSeatBookingsByFlightId(Long flightId) {
-        return seatBookingRepository.findByFlightId(flightId);
-    }
+  public List<SeatBooking> getAllSeatBookingsByFlightId(Long flightId) {
+    return seatBookingRepository.findByFlightId(flightId);
+  }
+
+  public Map<Long, List<SeatBooking>> getSeatBookingsForFlights(List<Long> flightIds) {
+    Map<Long, List<SeatBooking>> seatBookingsByFlight = new HashMap<>();
+
+    seatBookingRepository
+        .findAllById(flightIds)
+        .forEach(
+            sb -> {
+              seatBookingsByFlight
+                  .computeIfAbsent(sb.getFlight().getId(), k -> new ArrayList<>())
+                  .add(sb);
+            });
+
+    return seatBookingsByFlight;
+  }
 }
