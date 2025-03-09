f4521302cb16a91f1e2b3bf14f4189e2477d6f09
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-09 17:00:39 +0200

    refactor(dto): rename 'model' to 'producer' in PlaneResponseDTO

--- backend/src/main/java/com/cgi/flights/dto/response/PlaneResponseDTO.java
+++ backend/src/main/java/com/cgi/flights/dto/response/PlaneResponseDTO.java
@@ -2,4 +2,4 @@ package com.cgi.flights.dto.response;
 
 import java.util.List;
 
-public record PlaneResponseDTO(Long id, String name, String model, List<SeatResponseDTO> seats) {}
+public record PlaneResponseDTO(Long id, String name, String producer, List<SeatResponseDTO> seats) {}
